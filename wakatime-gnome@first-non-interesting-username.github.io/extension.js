d839aa526931145e0da30facaa6e8427609b4bb3
First-Non-Interesting-Username <janekmusin@proton.me>
2026-09-05 19:45:41 +0200

    feat: add fetching

--- wakatime-gnome@first-non-interesting-username.github.io/extension.js
+++ wakatime-gnome@first-non-interesting-username.github.io/extension.js
@@ -16,38 +16,86 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-import GObject from 'gi://GObject';
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
-import Soup from 'gi://Soup?version=3.0';
+import GObject from "gi://GObject";
+import St from "gi://St";
+import Clutter from "gi://Clutter";
+import Soup from "gi://Soup?version=3.0";
+import GLib from "gi://GLib";
 
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import {
+  Extension,
+  gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+
+const session = new Soup.Session();
+const api_key = "";
+
+async function getToday() {
+  const message = Soup.Message.new(
+    "GET",
+    `https://hackatime.hackclub.com/api/hackatime/v1/users/my/statusbar/today?api_key=${api_key}`,
+  );
+
+  const bytes = await session.send_and_read_async(
+    message,
+    GLib.PRIORITY_DEFAULT,
+    null,
+  );
+
+  if (message.get_status() !== Soup.Status.OK)
+    throw new Error(`HTTP ${message.get_status()}`);
+
+  const text = new TextDecoder().decode(bytes.get_data());
+  const json = JSON.parse(text);
+  return json.data.grand_total.text;
+}
 
 const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
-  _init() {
-      super._init(0.0, _('Wakatime gnome indicator'));
+  class Indicator extends PanelMenu.Button {
+    _init() {
+      super._init(0.0, _("Wakatime gnome indicator"));
       this._label = new St.Label({
-        text: 'Server unavailable',
+        text: "Loading...",
         x_align: Clutter.ActorAlign.CENTER,
         y_align: Clutter.ActorAlign.CENTER,
       });
       this.add_child(this._label);
-  }
-});
+      this.refresh();
+    }
+    async refresh() {
+      try {
+        const text = await getToday();
+        this._label.set_text(text);
+      } catch (e) {
+        console.error(this.uuid, e);
+        this._label.set_text("Server unavailable");
+      }
+    }
+  },
+);
 
 export default class IndicatorExampleExtension extends Extension {
-    enable() {
-        this._indicator = new Indicator();
-        Main.panel.addToStatusArea(this.uuid, this._indicator);
+  enable() {
+    this._indicator = new Indicator();
+    Main.panel.addToStatusArea(this.uuid, this._indicator);
+    this._timer = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
+      this._indicator?.refresh();
+      return GLib.SOURCE_CONTINUE;
+    });
+  }
+
+  disable() {
+    if (this._timer) {
+      GLib.source_remove(this._timer);
+      this._timer = null;
     }
 
-    disable() {
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+    this._indicator.destroy();
+    this._indicator = null;
+    session.abort();
+  }
 }
