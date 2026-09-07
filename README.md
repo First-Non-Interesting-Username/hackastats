9f90d222ef5c9341288eeda5dbe4ecd65d46f489
First-Non-Interesting-Username <janekmusin@proton.me>
2026-09-07 21:15:55 +0200

    docs: start README

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # wakastats
 
-A GNOME
+![Image of GNOME shell with wakastats extension enabled](wakastats.png)
+
+A simple GNOME shell extension displaying wakatime today data
+
+## Installations
+
+This extension supports only GNOME 50+
+Check your GNOME version with
+
+```bash
+gnome-shell --version
+```
+
+<!-- I'm waiting for this extension to be approved on https://extensions.gnome.org/ -->
+
+## Configuration
+
+## Developement setup
 
 ## Disclaimer
 
--- devenv.nix
+++ devenv.nix
@@ -6,12 +6,17 @@
   };
 
   scripts = {
+    # Run nested GNOME session
     "run-gnome".exec = "dbus-run-session -- gnome-shell --devkit";
+    # Link the extension for developement
     "link-extension".exec = ''
       ln -sfn \
         "$PWD/wakastats@first-non-interesting-username.github.io" \
         "$HOME/.local/share/gnome-shell/extensions/wakastats@first-non-interesting-username.github.io"
     '';
+    # Revert linking
+    "unlink-extension".exec = "rm ~/.local/share/gnome-shell/extensions/wakastats@first-non-interesting-username.github.io";
+    # Compile schemas
     "compile-schemas".exec = "glib-compile-schemas wakastats@first-non-interesting-username.github.io/schemas/";
   };
   packages = with pkgs; [
